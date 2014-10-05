958e32d209c2d9d7f32282a6135222205f59bcee
lamazavr <mr.dimas@meta.ua>
2014-10-05 18:26:28 +0300

    Добавил комментарии

--- Uart.h
+++ Uart.h
@@ -10,12 +10,26 @@
 
 #include "DSP2802x_Device.h"
 
+/**
+ * Инициализация передатчика
+ */
 void Uart_init();
 
+/**
+ * Инициализация ножек для Uart
+ */
 void Uart_init_gpio();
 
+/**
+ * Посылка байта
+ * @param a - Отсылаемый байт
+ */
 void Uart_send(Uint16 a);
 
+/**
+ * Посылка строки
+ * @param msg - Указатель на строку
+ */
 void Uart_send_msg(char *msg);
 
 
